72b43a86fb1c20a6cd94994604d42bbd601c9e5f
muskan-67 <guptamuskan1267@gmail.com>
2026-08-18 09:58:34 +0530

    photo update

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { motion } from "framer-motion";
-import myImage from "./assets/image/my1.jpg";
+import myImage from "./assets/image/pink.jpeg";
 
 const Hero = () => {
   return (
